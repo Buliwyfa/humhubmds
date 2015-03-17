245aefe2b8ec9557b1e174c84f668ce210c3db4d
grandiox <diego.alvarez.info@gmail.com>
2015-03-17 09:49:26 -0300

    Create chat.php

---
+++ chat.php
@@ -0,0 +1,211 @@
+<?php
+		$servidor   = 'mdss025';
+        $nombreBD   = 'socialmds';
+        $usuario    = 'socialmds';
+        $contrasena = 'social123';
+
+$dbh = mysqli_connect($servidor, $usuario, $contrasena, $nombreBD);
+
+/* verificar la conexión */
+if (mysqli_connect_errno()) {
+    printf("Conexión fallida: %s\n", mysqli_connect_error());
+    exit();
+}
+
+session_start();
+
+if ($_GET['action'] == "chatheartbeat") { chatHeartbeat(); } 
+if ($_GET['action'] == "sendchat") { sendChat(); } 
+if ($_GET['action'] == "closechat") { closeChat(); } 
+if ($_GET['action'] == "startchatsession") { startChatSession(); } 
+
+if (!isset($_SESSION['chatHistory'])) {
+	$_SESSION['chatHistory'] = array();	
+}
+
+if (!isset($_SESSION['openChatBoxes'])) {
+	$_SESSION['openChatBoxes'] = array();	
+}
+
+function chatHeartbeat() {
+	global $dbh;
+	$username=mysqli_real_escape_string($dbh,$_COOKIE['username']);
+	
+	$sql = "SELECT * FROM `chat` WHERE recd=0 and chat.to = '".$username."' ORDER BY id ASC";
+	$query = mysqli_query($dbh,$sql);
+	$items = '';
+
+	$chatBoxes = array();
+
+	while ($chat = mysqli_fetch_array($query)) {
+
+		if (!isset($_SESSION['openChatBoxes'][$chat['from']]) && isset($_SESSION['chatHistory'][$chat['from']])) {
+			$items = $_SESSION['chatHistory'][$chat['from']];
+		}
+
+		$chat['message'] = sanitize($chat['message']);
+
+		$items .= <<<EOD
+					   {
+			"s": "0",
+			"f": "{$chat['from']}",
+			"m": "{$chat['message']}"
+	   },
+EOD;
+
+	if (!isset($_SESSION['chatHistory'][$chat['from']])) {
+		$_SESSION['chatHistory'][$chat['from']] = '';
+	}
+
+	$_SESSION['chatHistory'][$chat['from']] .= <<<EOD
+						   {
+			"s": "0",
+			"f": "{$chat['from']}",
+			"m": "{$chat['message']}"
+	   },
+EOD;
+		
+		unset($_SESSION['tsChatBoxes'][$chat['from']]);
+		$_SESSION['openChatBoxes'][$chat['from']] = $chat['sent'];
+	}
+
+	if (!empty($_SESSION['openChatBoxes'])) {
+	foreach ($_SESSION['openChatBoxes'] as $chatbox => $time) {
+		if (!isset($_SESSION['tsChatBoxes'][$chatbox])) {
+			$now = time()-strtotime($time);
+			$time = date('M j, H:i', strtotime($time));
+
+			$message = "Enviado $time";
+			if ($now > 180) {
+				$items .= <<<EOD
+{
+"s": "2",
+"f": "$chatbox",
+"m": "{$message}"
+},
+EOD;
+
+	if (!isset($_SESSION['chatHistory'][$chatbox])) {
+		$_SESSION['chatHistory'][$chatbox] = '';
+	}
+
+	$_SESSION['chatHistory'][$chatbox] .= <<<EOD
+		{
+"s": "2",
+"f": "$chatbox",
+"m": "{$message}"
+},
+EOD;
+			$_SESSION['tsChatBoxes'][$chatbox] = 1;
+		}
+		}
+	}
+}
+
+	$sql = "update chat set recd = 1 where chat.to = '".$username."' and recd = 0";
+	$query = mysqli_query($dbh,$sql);
+
+	if ($items != '') {
+		$items = substr($items, 0, -1);
+	}
+header('Content-type: application/json');
+?>
+{
+		"items": [
+			<?php echo $items;?>
+        ]
+}
+
+<?php
+			exit(0);
+}
+
+function chatBoxSession($chatbox) {
+	
+	$items = '';
+	
+	if (isset($_SESSION['chatHistory'][$chatbox])) {
+		$items = $_SESSION['chatHistory'][$chatbox];
+	}
+
+	return $items;
+}
+
+function startChatSession() {
+	$items = '';
+	if (!empty($_SESSION['openChatBoxes'])) {
+		foreach ($_SESSION['openChatBoxes'] as $chatbox => $void) {
+			$items .= chatBoxSession($chatbox);
+		}
+	}
+
+
+	if ($items != '') {
+		$items = substr($items, 0, -1);
+	}
+
+header('Content-type: application/json');
+?>
+{
+		"username": "<?php echo $_COOKIE['username'];?>",
+		"items": [
+			<?php echo $items;?>
+        ]
+}
+
+<?php
+
+
+	exit(0);
+}
+
+function sendChat() {
+global $dbh;
+	$from = $_COOKIE['username'];
+	$to = $_POST['to'];
+	$message = $_POST['message'];
+
+	$_SESSION['openChatBoxes'][$_POST['to']] = date('Y-m-d H:i:s', time());
+	
+	$messagesan = sanitize($message);
+
+	if (!isset($_SESSION['chatHistory'][$_POST['to']])) {
+		$_SESSION['chatHistory'][$_POST['to']] = '';
+	}
+
+	$_SESSION['chatHistory'][$_POST['to']] .= <<<EOD
+					   {
+			"s": "1",
+			"f": "{$to}",
+			"m": "{$messagesan}"
+	   },
+EOD;
+
+
+	unset($_SESSION['tsChatBoxes'][$_POST['to']]);
+	
+	$from1=mysqli_real_escape_string($dbh,$from);
+	$to1=mysqli_real_escape_string($dbh,$to);
+	$mess1=mysqli_real_escape_string($dbh,$message);
+	
+	$sql = "insert into chat (chat.from,chat.to,message,sent) values ('".$from1."', '".$to1."','".$mess1."',NOW())";
+	$query = mysqli_query($dbh,$sql);
+	echo "1";
+	exit(0);
+}
+
+function closeChat() {
+
+	unset($_SESSION['openChatBoxes'][$_POST['chatbox']]);
+	
+	echo "1";
+	exit(0);
+}
+
+function sanitize($text) {
+	$text = htmlspecialchars($text, ENT_QUOTES);
+	$text = str_replace("\n\r","\n",$text);
+	$text = str_replace("\r\n","\n",$text);
+	$text = str_replace("\n","<br>",$text);
+	return $text;
+}
